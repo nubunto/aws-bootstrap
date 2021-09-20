fcefd861a2fcc8b4f64fc8c5365fde222e0e6efb
Bruno Silva <bruno.silva@loadsmart.com>
2021-09-19 22:59:26 -0300

    automatic deploys!

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -4,9 +4,12 @@ STACK_NAME=awsbootstrap
 REGION=us-east-1
 CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
-    --query "Account" --output text`
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
 
 # Deploys static resources
 echo -e "\n\n=========== Deploying setup.yml ==========="
@@ -30,7 +33,12 @@ aws cloudformation deploy \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-      EC2InstanceType=$EC2_INSTANCE_TYPE
+      EC2InstanceType=$EC2_INSTANCE_TYPE \
+      GitHubOwner=$GH_OWNER \
+      GitHubRepo=$GH_REPO \
+      GitHubBranch=$GH_BRANCH \
+      GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+      CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 if [ $? -eq 0 ]; then
     aws cloudformation list-exports \
