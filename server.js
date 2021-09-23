c400ac5318c83d71464f1a6228925636781580f9
Bruno Silva <bruno.silva@loadsmart.com>
2021-09-22 23:41:44 -0300

    add stack name to server output

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const { hostname } = require('os'); 
 
+const STACK_NAME = process.env.STACK_NAME || 'unknown env'
+
 const http = require('http'); 
-const message = `Hello Cloud from ${hostname()}!\n`; 
+const message = `Hello Cloud from ${hostname()} in ${STACK_NAME}!\n`; 
 const port = 8080;
 const server = http.createServer((req, res) => { 
   res.statusCode = 200; 
--- start-service.sh
+++ start-service.sh
@@ -2,4 +2,14 @@
 
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
+
+REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
+
+# query the instance metadata API to get the instance ID
+export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+export STACK_NAME=`aws --region $REGION ec2 describe-tags \
+    --filters "Name=resource-id,Values=${INSTANCE_ID}" \
+              "Name=key,Values=aws:cloudformation:stack-name" \
+    | jq -r ".Tags[0].Value"`
+
 npm run start
